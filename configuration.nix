96eb9b318006369d7a5541585ce53b6ecf20bfcd
raf <rraf@tuta.io>
2024-03-10 14:28:43 +0000

    chore: customised sudo

--- configuration.nix
+++ configuration.nix
@@ -35,6 +35,12 @@ in
     enable = true;
     execWheelOnly = true;
     wheelNeedsPassword = true;
+    extraConfig = ''
+      Defaults pwfeedback
+      Defaults timestamp_timeout = 0
+      Defaults lecture = always
+      Defaults passprompt = "> "
+    '';
   };
 
   # timezone, networking, locale

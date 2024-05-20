1f5f5d1c695ca7ab9dd68686cd8f64a7e7cce464
raf <rraf@tuta.io>
2024-05-20 10:44:43 +0100

    chore: removed darkreader

--- home.nix
+++ home.nix
@@ -198,7 +198,6 @@ in
             sponsorblock
             youtube-recommended-videos
             scroll_anywhere
-            darkreader
           ];
           userChrome = ''
             #TabsToolbar { visibility: collapse !important; }

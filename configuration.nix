7f16a914c5c72ab625b596073f64568eb8c4ce8d
raf <rraf@tuta.io>
2024-09-11 06:01:40 +0100

    chore: refactor

--- configuration.nix
+++ configuration.nix
@@ -8,10 +8,21 @@ let
   home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/release-${STATE_VERSION}.tar.gz";
 in
 {
+  nixpkgs.config = {
+    allowUnfree = true;
+    packageOverrides = pkgs: {
+      nur = import (fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {
+        inherit pkgs;
+      };
+      unstable = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz") {
+        inherit pkgs;
+      };
+    };
+  };
+
   imports = [
     "${nixos-hardware}/framework/13-inch/7040-amd"
     "${home-manager}/nixos"
-
     ./modules/linux
     ./modules/environment
     ./modules/home
@@ -20,59 +31,6 @@ in
     ./modules/fonts
     ./modules/formatters
   ];
-
-  users = {
-    defaultUserShell = pkgs.fish;
-    users.${USER} = {
-      isNormalUser = true;
-      description = "user";
-      extraGroups = [
-        "networkmanager"
-        "wheel"
-        "input"
-        "docker"
-      ];
-    };
-  };
-
-  home-manager.users.${USER}.home.stateVersion = "${STATE_VERSION}";
-  system = {
-    stateVersion = "${STATE_VERSION}";
-    activationScripts.dotfiles = {
-      text = ''
-        if [ -L /root/.config ]; then
-            rm /root/.config
-        elif [ -d /root/.config ]; then
-            rm -rf /root/.config
-        fi
-        ln -s /home/${USER}/.config /root/.config
-        chown -R ${USER} /home/${USER}/.config
-      '';
-    };
-  };
-
-  i18n = {
-    defaultLocale = "en_GB.UTF-8";
-    extraLocaleSettings = {
-      LC_ADDRESS = "en_GB.UTF-8";
-      LC_IDENTIFICATION = "en_GB.UTF-8";
-      LC_MEASUREMENT = "en_GB.UTF-8";
-      LC_MONETARY = "en_GB.UTF-8";
-      LC_NAME = "en_GB.UTF-8";
-      LC_PAPER = "en_GB.UTF-8";
-      LC_TELEPHONE = "en_GB.UTF-8";
-    };
-  };
-
-  time.timeZone = "Europe/London";
-
-  networking = {
-    hostName = "FRMWRK";
-    networkmanager = {
-      enable = true;
-      wifi.backend = "wpa_supplicant";
-    };
-  };
 
   boot = {
     kernelModules = [ "cpufreq_ondemand" ];
@@ -94,6 +52,57 @@ in
       };
     };
     supportedFilesystems = [ "ntfs" ];
+  };
+
+  networking = {
+    hostName = "FRMWRK";
+    networkmanager = {
+      enable = true;
+      wifi.backend = "wpa_supplicant";
+    };
+  };
+
+  time.timeZone = "Europe/London";
+  i18n = {
+    defaultLocale = "en_GB.UTF-8";
+    extraLocaleSettings = {
+      LC_ADDRESS = "en_GB.UTF-8";
+      LC_IDENTIFICATION = "en_GB.UTF-8";
+      LC_MEASUREMENT = "en_GB.UTF-8";
+      LC_MONETARY = "en_GB.UTF-8";
+      LC_NAME = "en_GB.UTF-8";
+      LC_PAPER = "en_GB.UTF-8";
+      LC_TELEPHONE = "en_GB.UTF-8";
+    };
+  };
+
+  users = {
+    defaultUserShell = pkgs.fish;
+    users.${USER} = {
+      isNormalUser = true;
+      description = "user";
+      extraGroups = [
+        "networkmanager"
+        "wheel"
+        "input"
+      ];
+    };
+  };
+
+  home-manager.users.${USER}.home.stateVersion = "${STATE_VERSION}";
+  system = {
+    stateVersion = "${STATE_VERSION}";
+    activationScripts.dotfiles = {
+      text = ''
+        if [ -L /root/.config ]; then
+            rm /root/.config
+        elif [ -d /root/.config ]; then
+            rm -rf /root/.config
+        fi
+        ln -s /home/${USER}/.config /root/.config
+        chown -R ${USER} /home/${USER}/.config
+      '';
+    };
   };
 
   services = {
